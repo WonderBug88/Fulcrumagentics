be1b1f8780f982df7ffe32c5964e250ad7cc508f
JD <114538432+WonderBug88@users.noreply.github.com>
2025-12-23 11:25:22 -0700

    Remove server settings and integrations
    Removed server configuration and integrations from astro.config.ts.

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
 export default defineConfig({
   site: "https://fulcrumagentics.com",
   base: "/",
-  integrations: [icon()],
-  server: {
-    host: '0.0.0.0',
-    port: 5000,
-    allowedHosts: true, // don't touch this!
-  },
-});
-
 
